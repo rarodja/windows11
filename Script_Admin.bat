50132c97811408a4edacc29a27f1f8f6dcebf493
Rafa <74628132+rarodja@users.noreply.github.com>
2023-05-03 12:13:35 +0200

    Add files via upload

---
+++ Script_Admin.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+PowerShell -Command "Get-AppxPackage *Microsoft.WindowsAlarms* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.WindowsCamera* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.WindowsSoundRecorder* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.XboxGamingOverlay* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.WindowsFeedbackHub* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Clipchamp.Clipchamp* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.BingNews* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.WindowsMaps* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.Todos* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.MicrosoftSolitaireCollection* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.MicrosoftStickyNotes* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.GetHelp* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.MicrosoftOfficeHub* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.Paint* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.ZuneVideo* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.Getstarted* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.549981C3F5F10* | Remove-AppxPackage"
+PAUSE
+EXIT
